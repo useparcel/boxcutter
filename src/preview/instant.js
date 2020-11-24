1db477fd1ba7376cce269d442605c785c68230cd
Avi Goldman <avrahamymgoldman@gmail.com>
2020-11-24 12:15:34 -0500

    instant does a full iframe refresh on source id change

--- src/preview/instant.js
+++ src/preview/instant.js
@@ -1,70 +1,18 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { createIframe, waitUntilEvent } from "./iframe";
 import { useBoxcutterInternal, useBoxcutterEvent } from "../context";
 import useDOMParser from "../hooks/use-dom-parser";
 import useDiffDOM from "../hooks/use-diff-dom";
+import RefreshPreview from "./refresh";
+import useChangeEffect from "../hooks/use-change-effect";
 
-export default function InstantPreview({
-  frameId,
-  source,
-  title,
-  sandbox,
-  allow,
-  setIframe,
-}) {
-  const iframeRef = useRef();
-  const containerRef = useRef();
+export default function InstantPreview({ source, ...props }) {
   const dd = useDiffDOM();
   const domparser = useDOMParser();
   const domTree = useRef();
-  const { isLoading, set } = useBoxcutterInternal();
+  const { isLoading, set, emit } = useBoxcutterInternal();
 
-  /**
-   * create the initial iframe
-   */
-  useEffect(() => {
-    if (!iframeRef.current) {
-      const iframe = createIframe({ id: frameId });
-      containerRef.current.append(iframe);
-
-      iframeRef.current = iframe;
-      setIframe(iframeRef.current);
-    }
-
-    iframeRef.current.setAttribute("title", title);
-    iframeRef.current.setAttribute("sandbox", sandbox);
-    iframeRef.current.setAttribute("allow", allow);
-    iframeRef.current.publishEvent("setFrameId", frameId);
-  }, [frameId, title, sandbox, allow, setIframe]);
-
-  /**
-   * Write the initial content
-   */
-  useEffect(() => {
-    waitUntilEvent(frameId, "DOMContentLoaded").then(() => {
-      iframeRef.current.publishEvent("write", source.html);
-      set((c) => ({ ...c, isLoading: false }));
-      iframeRef.current.publishEvent("scroll", {
-        scrollTop: 0,
-        scrollLeft: 0,
-      });
-    });
-  }, []);
-
-  /**
-   * scroll to top and reset dom tree when the source changes
-   *
-   * the preview handler will also be triggered and it will write the html
-   */
-  useEffect(() => {
-    if (iframeRef.current) {
-      domTree.current = null;
-      iframeRef.current.publishEvent("scroll", {
-        scrollTop: 0,
-        scrollLeft: 0,
-      });
-    }
-  }, [source.id]);
+  const [controlledSource, setControlledSource] = useState(source);
 
   /**
    * update preview handler
@@ -83,15 +31,23 @@ export default function InstantPreview({
      * Otherwise, just send the iframe the diff to be applied
      */
     if (!domTree.current) {
-      iframeRef.current.publishEvent("write", source.html);
+      emit("write", source.html);
     } else {
       const diff = dd.diff(domTree.current, newTree);
 
-      iframeRef.current.publishEvent("update", diff);
+      emit("update", diff);
     }
 
     domTree.current = newTree;
   }, [isLoading, source.id, source.html, dd, domparser]);
 
-  return <div ref={containerRef} style={{ height: "100%", width: "100%" }} />;
+  useChangeEffect(
+    () => {
+      setControlledSource(source);
+    },
+    [source.id],
+    [source.id, source.html]
+  );
+
+  return <RefreshPreview source={controlledSource} {...props} />;
 }
